7fb2847836486356c3c90f63f50213ffb8c1e9c1
Matt <matthew-casey@lambdastudents.com>
2021-04-06 23:31:39 -0500

    finished everything except task 5

--- index.js
+++ index.js
@@ -28,9 +28,10 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  counter 1 has local scope and counter2 has global scope
   
   2. Which of the two uses a closure? How can you tell?
-  
+   both funtions use closure
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -62,8 +63,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * 3)
 }
 
 
@@ -81,17 +82,36 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
-}
+function finalScore(inningCB,numOfInnings){
+   let scores = {
+     'Home': 0,
+     'Away': 0
+   }
+    for (let i = 0; i < numOfInnings; i++){
+      scores['Home'] += inningCB();
+      scores['Away'] += inningCB();
+    }
+    return scores
+  }
+ 
+  
+
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  let scores = {
+    'Home': 0,
+    'Away': 0
+  }
+     scores['Home'] = inningCB();
+     scores['Away'] = inningCB();
+   
+   return scores
 }
 
 
@@ -136,8 +156,8 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningCB,inningCB,numOfInnings) {
+
 }
 
 
